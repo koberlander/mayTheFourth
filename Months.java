56daf784877ee1a566284cf47a33bd87dfec4c79
Kristin Oberlander <koberlander115@gmail.com>
2018-05-05 10:58:01 -0400

    Update Months.java
    Condensed months array initialization

--- Months.java
+++ Months.java
@@ -2,19 +2,7 @@ package labs;
 
 public class Months {
 	public static void main(String[] args) {
-	String[] months = new String[12];
-		months[0]="January";
-		months[1]="February";
-		months[2]="March";
-		months[3]="April";
-		months[4]="May";
-		months[5]="June";
-		months[6]="July";
-		months[7]="August";
-		months[8]="September";
-		months[9]="October";
-		months[10]="November";
-		months[11]="December";
+		String[] months = {"January","February","March","April","May","June","July","August","September","October","November","December"};
 		
 		System.out.println(months[4]);
 }
